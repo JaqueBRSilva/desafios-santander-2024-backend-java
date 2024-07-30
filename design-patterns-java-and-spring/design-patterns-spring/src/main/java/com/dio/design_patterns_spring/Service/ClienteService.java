a0d8c3571cf048a4f524c369f95d47a986d90164
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-07-30 15:38:51 -0300

    feat: adicionado projeto 'design-patterns-spring'

---
+++ design-patterns-java-and-spring/design-patterns-spring/src/main/java/com/dio/design_patterns_spring/Service/ClienteService.java
@@ -0,0 +1,23 @@
+package com.dio.design_patterns_spring.Service;
+
+import com.dio.design_patterns_spring.Model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ */
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
